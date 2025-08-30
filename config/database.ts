604fcf168a9586f97655d4c63c1429def5f9d47e
fernan23 <fdsoria23@gmail.com>
2025-08-30 01:06:23 -0300

    more changes database.ts

--- config/database.ts
+++ config/database.ts
@@ -1,8 +1,8 @@
-import path from 'path';
+/*  import path from 'path';
 
 export default ({ env }) => {
   const client = env('DATABASE_CLIENT', 'postgres');
-
+  
   const connections = {
     mysql: {
       connection: {
@@ -18,15 +18,15 @@ export default ({ env }) => {
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
+          },
+          },
+          pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+          },
+          postgres: {
+            connection: {
+              connectionString: env('DATABASE_URL'),
+              host: env('DATABASE_HOST', 'localhost'),
+              port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'ecommerce'),
         user: env('DATABASE_USERNAME', ''),
         password: env('DATABASE_PASSWORD', ''),
@@ -37,27 +37,43 @@ export default ({ env }) => {
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
-  };
+          },
+          schema: env('DATABASE_SCHEMA', 'public'),
+          },
+          pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+          },
+          sqlite: {
+            connection: {
+              filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+              },
+              useNullAsDefault: true,
+              },
+              };
+              
+              return {
+                connection: {
+                  client,
+                  ...connections[client],
+                  acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+                  },
+                  };
+                  }; 
+                  
+ */
 
-  return {
+                  export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      connectionString: env('DATABASE_URL'),
+      ssl: {
+        rejectUnauthorized: false, // Necesario para Render
+      },
     },
-  };
-};
-
-
-
+    pool: {
+      min: env.int('DATABASE_POOL_MIN', 2),
+      max: env.int('DATABASE_POOL_MAX', 10),
+    },
+    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+  },
+});
